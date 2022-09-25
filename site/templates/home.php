12a767d5c011a92b2f1001922e1f57f73d1b4bc5
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2022-09-25 17:04:56 +0200

    init commit

---
+++ site/templates/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PresentOnline</title>
+
+        <!-- STYLESHEETS -->
+        <?= css('build/css/style.css') ?>
+
+        <!-- GOOGLE FONTS -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Home</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum elit sapien, aliquet eu ullamcorper quis, congue fringilla tortor. Integer eu justo at magna vulputate ornare. Nulla condimentum enim venenatis leo iaculis dignissim. Ut quis libero ante. Ut faucibus felis in enim aliquam efficitur. Aenean non sapien vel lacus sollicitudin dignissim non lacinia dolor. Sed at ligula vulputate, elementum dui et, sagittis nisi. Sed venenatis lacus in gravida pharetra. Nulla facilisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam dui nisl, gravida placerat aliquet vel, efficitur eu velit. Curabitur eu metus malesuada, euismod purus in, dictum sem. Aliquam laoreet congue neque non eleifend. Proin dapibus eget odio quis tincidunt. Vestibulum vulputate varius bibendum. In id velit blandit, luctus dolor a, tempus nunc.</p>
+    </body>
+</html>
